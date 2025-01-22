82e553fe6533389c92c4d4ec7b987b88a13cd702
alexshenker <alekseyshenker@gmail.com>
2025-01-22 11:52:39 -0500

    button styled to look more like solace buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,13 +67,13 @@ export default function Home() {
           Searching for: <span id="search-term"></span>
         </p>
         <input
-          style={{ border: "1px solid black" }}
           value={searchTerm}
           onChange={onChange}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-visible:outline-none focus:ring-teal-500 focus:border-teal-500 p-2.5 inline-block"
         />
         <button
           onClick={reset}
-          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium active:bg-gray-200 rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+          className="text-white bg-teal-800 border border-gray-300 focus:outline-none hover:bg-teal-900 font-medium active:bg-teal-950 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mx-3 inline-block"
         >
           Reset Search
         </button>
